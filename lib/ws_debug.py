cd0815a64280261d1f0fe25ac9926213b31d6e01
Brendan Kristiansen <b@bek.sh>
2019-10-19 15:10:21 -0600

    Create debug module

---
+++ lib/ws_debug.py
@@ -0,0 +1,16 @@
+## The Work Scheduler
+## Montana State University senior design project
+##
+## Copyright 2019
+##
+## Created: 2019-10-19 by Brendan Kristiansen
+## ws_debug.py
+## Debugging tools for Work Scheduler
+
+if __name__ == "__main__":
+    print("Unable to execute as script")
+    exit(-1)
+
+# Print debug message to console
+def ws_debug_msg(msg):
+    print("WS:", msg)
