39913805a2aaaa681d3953ca2cb2ef56e7b258bd
Brendan Kristiansen <b@bek.sh>
2019-10-19 11:27:10 -0600

    Call Qt Example in driver

--- src/work_scheduler.py
+++ src/work_scheduler.py
@@ -9,10 +9,18 @@
 ## work_scheduler.py
 ## Driver for The Work Scheduler
 
+import sys
+from PyQt5.QtWidgets import QApplication
+from lib import ws_gui
+
+app = None
+
 def main():
-    pass
+    ex = ws_gui.Example()
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
+    app = QApplication(sys.argv)
     main()
 else:
     print("Unable to import as module")
