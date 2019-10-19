158b1b2207585c56377c27706b091c85b727e383
Brendan Kristiansen <b@bek.sh>
2019-10-19 10:54:54 -0600

    Check in main driver

---
+++ src/work_scheduler.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+## The Work Scheduler
+## Montana State University senior design project
+##
+## Copyright 2019
+##
+## Created: 2019-10-19 by Brendan Kristiansen
+## work_scheduler.py
+## Driver for The Work Scheduler
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+else:
+    print("Unable to import as module")
