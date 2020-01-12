8a915ced1650dd8cde95ec016be7807740593e32
Brendan Kristiansen <b@bek.sh>
2020-01-12 16:12:44 -0700

    Check in drafted functions.

--- lib/ws_db.py
+++ lib/ws_db.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     print("Unable to execute as script")
     exit(-1)
 
-#import mysql.connector as sql
+import mysql.connector as sql
 
 from lib import CONSTANTS as K
 from lib import ws_gui
@@ -19,12 +19,25 @@ from lib import ws_gui
 # Manages connection with mySQL database
 class DB_Connection:
 
+    cnx = None
+
     def __init__(self):
         pass
 
     # Login to database. Store open connection
     def db_login(self, ex):
         srv_addr, uname, pw = ex.login()
+        self.cnx = sql.connect(user=uname, password=pw, host=srv_addr, database='tws')
+        print(self.cnx)
+        print("Connected to database!")
+
+    def init_db(self):
+        if self.crs == None or self.cnx == None:
+            return
+        else:
+            self.db_command("CREATE TABLE IF NOT EXISTS employee (eid int, employee_name character, rank int, role character, hourly_rate real);")
+            self.db_command("CREATE TABLE IF NOT EXISTS project (pid int, project_name character, estimated_hrs real, start_date date, end_date date, rpt int);")
+
 
     # Create a database user.
     def create_user(self):
