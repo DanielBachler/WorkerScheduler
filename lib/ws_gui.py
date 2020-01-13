744b9a30378c5e81c2278a4f804b06fafdb9d9a6
DanielBachler <daniel.bachler@comcast.net>
2020-01-12 19:35:49 -0700

    Cleaning up code, adding comments, new menu options

--- lib/ws_gui.py
+++ lib/ws_gui.py
@@ -37,23 +37,33 @@ class Main_UI(QMainWindow):
 
     def initUI(self):
         size = (600, 600)
-        # Setup main display, needs: Pane of worker names to chose (left), pane showing selected worker info (right),
-        # menu: exit, edit selected worker?,
+        # Setup main display: Pane of worker names to chose (left), pane showing selected worker info (right),
+        # menu: exit, add new user (new window),
+        # Buttons: delete selected user (prompt), edit selected user (new window)
 
         # Create menu bar for options
         menubar = self.menuBar()
-        # Exit submenu
-        exitMenu = menubar.addMenu('Exit')
+        # Menu bar tabs
+        fileMenu = menubar.addMenu('File')
+        userMenu = menubar.addMenu('Users')
 
-        # Actions for exit menu
-        # Setup exit action
+        # Actions for file menu
         exitAction = QAction('Exit Application', self)
         exitAction.triggered.connect(self.close)
 
         saveAction = QAction('Save', self)
-        # Add actions to menu
-        exitMenu.addAction(exitAction)
-        exitMenu.addAction(saveAction)
+        saveAction.triggered.connect(self.save)
+
+        # Actions for user menu
+        newUserAction = QAction('Add New User', self)
+        newUserAction.triggered.connect(self.makeNewUser)
+
+        # Add actions to file menu
+        fileMenu.addAction(saveAction)
+        fileMenu.addAction(exitAction)
+
+        # Add actions to user menu
+        userMenu.addAction(newUserAction)
 
         # Create contents of window
         centralWidget = QWidget()
@@ -72,15 +82,16 @@ class Main_UI(QMainWindow):
         # Pane left
         testEmployees = ("Dan", "Jesse", "Brendan", "Ed")
         employees = QListWidget()
-        employees.setFixedSize(80, 80)
+        #employees.setFixedSize(80, 80)
         for i in range(0, len(testEmployees)):
             employees.addItem(testEmployees[i])
         vboxL.addWidget(employees)
 
         # Pane Right
         employee = QLineEdit()
-        employee.setFixedSize(80, 80)
-        employee.setAlignment(Qt.AlignRight)
+        # employee.setFixedSize(80, 80)
+        employee.setFixedHeight(employees.height())
+        employee.setAlignment(Qt.AlignLeft)
         employee.setReadOnly(True)
         vboxR.addWidget(employee)
 
@@ -101,6 +112,9 @@ class Main_UI(QMainWindow):
         self.setWindowTitle('Worker Scheduler')
         self.show()
 
+    # closeEvent: changes the default closing behavior by overriding the base method
+    # ARGS: self (QMainWindow), event (a QEvent) which is in this case is one of the closing events
+    # RETURNS: none
     def closeEvent(self, event):
         temp = QMessageBox.question(self, 'Exit Confirmation', 'Are you sure you want to quit?',
                                     QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
@@ -110,5 +124,21 @@ class Main_UI(QMainWindow):
         else:
             event.ignore()
 
+    # newSelected: changes the displayed text in right side panel of QMainWindow
+    # ARGS: self (QMainWindow), item (a QListWidget List item)
+    # RETURNS: None
     def newSelected(self, item):
         self.centralWidget().findChild(QLineEdit).setText(item.text())
+
+    # makeNewUser: Creates a new user object and adds them to the database
+    # ARGS: self (QMainWindow)
+    # RETURNS: None, user object is added to local database instead
+    def makeNewUser(self):
+        # New popup window with ability to create new local user, push to database on close?
+        print("Add new user TODO")
+
+    # Save: saves the current state of local database to database server
+    # ARGS: self (QMainWindow)
+    # RETURNS: None
+    def save(self):
+        pass
